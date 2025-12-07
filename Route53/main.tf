160d0532cdd0b94a133d74e6eeee8ced222c8d04
Harshit <hsompura2012@gmail.com>
2025-12-07 18:32:18 +0530

    Update

--- Route53/main.tf
+++ Route53/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "main" {
   zone_id = var.zone_id
-  name    = "${componentname}-dev"
+  name    = "${var.componentname}-dev"
   type    = "A"
   ttl     = 50
   records = [var.private_ip]
